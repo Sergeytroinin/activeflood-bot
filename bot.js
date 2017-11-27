506340c84c8e9f346b39eea686d29eb9d9270ee8
Sergey <sergey4troinin@gmail.com>
2017-11-27 11:50:53 +0000

    Added chat functionality

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const fromElvesToRussian = require('./elves-translator');
 const config = require('./config.json');
 
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const token = config.token;
 const bot = new TelegramBot(token, {polling: true});
 
@@ -17,8 +21,10 @@ bot.on('message', (msg) => {
 
   const chatId = msg.chat.id;
 
+console.log('CHAT ID', chatId);
+
   if (msg.text && msg.text.toLowerCase().includes("флуд")) {
-    bot.sendMessage(msg.chat.id, "Хочешь пофлудить, " + msg.from.first_name + "?");
+    //bot.sendMessage(msg.chat.id, "Хочешь пофлудить, " + msg.from.first_name + "?");
   }
 
   let translated = fromElvesToRussian(msg.text);
@@ -27,4 +33,20 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, `Эльф в чате! Похоже он хотел сказать: ${translated}`);
   }
 
+});
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    bot.sendMessage(-1001123167251, msg);
+  });
 });
